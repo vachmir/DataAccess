5c678035b8ff7a68a7f5b0629c2f7e9c735a6498
vchgn <vchgn@VACHMIRLAPTOP>
2021-08-19 22:22:56 +0400

    Add project files.

---
+++ DataAccess/DataProviderEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    enum DataProviderEnum
+    {
+        SqlServer,
+    #if PC
+        OleDb,
+    #endif
+        Odbc,
+        //None
+    }
+}
---
+++ DataAccess/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+#if PC 
+  using System.Data.OleDb;
+#endif
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Very Simple Connection Factory *****\n");
+            Setup(DataProviderEnum.SqlServer);
+#if PC
+            Setup(DataProviderEnum.OleDb); //Not supported on macOS
+#endif
+            Setup(DataProviderEnum.Odbc);
+            //Setup(DataProviderEnum.None);
+            Console.ReadLine();
+        }
+
+        static void Setup(DataProviderEnum provider)
+        {
+            IDbConnection con =  GetConnection(provider);
+            Console.WriteLine($"Your connection is a {con.GetType().Name ?? "unrecognised type"}");
+        }
+
+       static IDbConnection GetConnection(DataProviderEnum dataProvider)
+      => dataProvider switch
+      {
+          DataProviderEnum.SqlServer => new SqlConnection(),
+       #if PC
+          DataProviderEnum.OleDb => new OleDbConnection(),
+       #endif
+          DataProviderEnum.Odbc=> new OdbcConnection(),
+          //_  =>null,
+        };
+    }
+}
