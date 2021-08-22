b787f7630c4c9a0791e6b930a5dc8fa9326bdc2d
Vachagan Mirzoian <vchgnmirms@gmail.com>
2021-08-22 21:47:11 +0400

    ConnectionStringBuilder

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -9,14 +9,16 @@ using System.IO;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
-namespace DataAccess
+namespace DataAccess 
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {        
+
             // DataProviderFactory();
-            SQLConnection();
+            //SQLConnection();
+            ConnectionStringBuilder();
         }
         static void DataProviderFactory()
         {
@@ -101,6 +103,35 @@ namespace DataAccess
                     }
                 }
             }
+        }
+
+        static void ConnectionStringBuilder()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                var connectionStringBuilder = new SqlConnectionStringBuilder
+                {
+                    InitialCatalog = "AutoLot",
+                    DataSource = "VachmirLaptop",
+                    IntegratedSecurity = true,
+                    ConnectTimeout = 30
+                };
+                connection.ConnectionString = connectionStringBuilder.ConnectionString;
+                connection.Open();
+                // Create a SQL command object.
+                string sql = @"Select i.id, m.Name as Make, i.Color, i.Petname FROM Inventory i INNER JOIN Makes m on m.Id = i.MakeId";
+                SqlCommand myCommand = new SqlCommand(sql, connection);
+                // Obtain a data reader a la ExecuteReader().
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    // Loop over the results.
+                    while (myDataReader.Read())
+                    {
+                        Console.WriteLine($"-> Make: {myDataReader["Make"]}, PetName: {myDataReader["PetName"]}, Color: {myDataReader["Color"]}.");
+                    }
+                }
+            }         
+
         }
     }
 }
